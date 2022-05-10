e183dcd2633e6dbfb003286fef35056b1271f5a5
Sooraj Sannabhadti <2005sooraj@gmail.com>
2022-05-10 20:34:17 +0800

    Create python_explanation.py

---
+++ python_explanation.py
@@ -0,0 +1,19 @@
+age = 6
+if age >= 3:
+    print("a is more than 3")
+if age > 5:   # blah
+    print("a is more than 5")
+    a = 1
+else:
+    print("a is less than 3")
+
+
+age = 32
+print("The age is: ", age)
+
+age2 = 21
+print("The first age is:", age , "The second age is:" , age2)
+
+
+a = input("What is the number? ")
+print(a)
